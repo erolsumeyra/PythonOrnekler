678b0ef7a18cde8ec44b281b5208b5f644aaa220
erolsumeyra <erolsumeyra@gmail.com>
2018-01-28 18:44:58 +0300

    Update metindekiKelimeSayisi.py

--- metindekiKelimeSayisi.py
+++ metindekiKelimeSayisi.py
@@ -1,13 +1,13 @@
 
-list=open("C:\\Users\USER\Desktop\kelimeler.txt","r")
-str=list.read().split('\n')
+kelime=open("C:\\Users\USER\Desktop\kelimeler.txt","r") #aradığımız kelimeler için kelime adında dosya oluşturur
+str=kelime.read().split('\n')
 print("\nMetinde Aranan Kelimeler:\n")
 print(str)
 print("\nMetindeki kelimelerimizin sayısı:\n")
-dosya=open('C:\\Users\SUMEYRA\Desktop\kitaplar\metin.txt',encoding="utf8")
+dosya=open('C:\\Users\USER\Desktop\kitaplar\metin.txt',encoding="utf8") #metnin bulunduğu dosyayı oluşturur
 r=dosya.read()
 i=0
-while(i<100):
+while(i<100): #döngüyü arayacağımız kelime sayısına göre başlatır
     x=r.count(str[i])
     print(str[i],"->",x)
     i+=1
